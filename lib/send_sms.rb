094e78abc1a2f5b4d1d201189d267c09d528bccb
“DovileSand” <dovile.sand@protonmail.ch>
2015-10-25 18:53:45 +0000

    Initial commit

---
+++ lib/send_sms.rb
@@ -0,0 +1,15 @@
+require 'twilio-ruby'
+
+account_sid = "AC9bbcc3e28bec6842697f872209e1db2d"
+
+
+begin
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client.account.messages.create({
+        :from => '+441668932043',
+        :to => '+447730478364',
+        :body => 'Hello world'
+    })
+rescue Twilio::REST::RequestError => e
+    puts e.message
+end
